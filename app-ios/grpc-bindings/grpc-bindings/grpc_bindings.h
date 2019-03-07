3929ef0d80ed20c1717d897ed14b42c5531d4831
Greg Rynkowski <dev@rynkowski.info>
2019-03-07 15:38:01 +0000

    Add auto-generated empty ObjC framework project for gRPC

---
+++ app-ios/grpc-bindings/grpc-bindings/grpc_bindings.h
@@ -0,0 +1,19 @@
+//
+//  grpc_bindings.h
+//  grpc-bindings
+//
+//  Created by Greg Rynkowski on 07/03/2019.
+//  Copyright © 2019 Greg Rynkowski. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for grpc_bindings.
+FOUNDATION_EXPORT double grpc_bindingsVersionNumber;
+
+//! Project version string for grpc_bindings.
+FOUNDATION_EXPORT const unsigned char grpc_bindingsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <grpc_bindings/PublicHeader.h>
+
+
