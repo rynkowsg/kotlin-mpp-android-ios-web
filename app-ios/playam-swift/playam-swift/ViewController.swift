4a01fb686f3eea0eac694ec43cb21dee22a43d53
Greg Rynkowski <dev@rynkowski.info>
2019-03-07 16:07:24 +0000

    Add auto-generated empty Swift app project

---
+++ app-ios/playam-swift/playam-swift/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  playam-swift
+//
+//  Created by Greg Rynkowski on 07/03/2019.
+//  Copyright © 2019 Greg Rynkowski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
