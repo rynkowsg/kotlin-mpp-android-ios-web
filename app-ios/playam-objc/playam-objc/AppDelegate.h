4a84fb6353e252fabdcb7b4c3128de781120eb88
Greg Rynkowski <dev@rynkowski.info>
2019-03-07 15:37:32 +0000

    Add auto-generated empty ObjC app project

---
+++ app-ios/playam-objc/playam-objc/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  playam-objc
+//
+//  Created by Greg Rynkowski on 07/03/2019.
+//  Copyright © 2019 Greg Rynkowski. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ app-ios/playam-objc/playam-objc/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  playam-objc
+//
+//  Created by Greg Rynkowski on 07/03/2019.
+//  Copyright © 2019 Greg Rynkowski. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
